9a66b2592e08a5ef044d7e0e219247695564c4f0
Kachan <luizkachan@gmail.com>
2022-04-23 11:02:40 -0300

    feature(detect_image_by_folder)

--- coletar_trigo.py
+++ coletar_trigo.py
@@ -1,57 +1,30 @@
 import pyautogui
 import time
-from python_imagesearch.imagesearch import imagesearch
+from python_imagesearch.imagesearch import imagesearch, imagesearch_from_folder
 import movements
 
 cursorposition = pyautogui.position()
 time.sleep(2)
 confidence = 0.9
+datafolder = "trigos/"
 
-for x in range(0, 10):
-    pos = imagesearch("./trigos/trigo1.png", confidence)
-    while pos[0] != -1:
-        print("trigo 1 in position : ", pos)
-        pyautogui.click(x=pos[0], y=pos[1], clicks=1, button='left')
-        pyautogui.moveTo(cursorposition)
-        time.sleep(6)
-        if imagesearch("./trigos/trigo1.png", confidence) != pos:
-            pos = imagesearch("./trigos/trigo1.png", confidence)
-        else:
+for x in range(0, 50):
+    found_flag = False
+    results = (imagesearch_from_folder(datafolder, 0.9))
+    for i in results:
+        if results[i] != [-1, -1]:
+            found_flag = True
             break
-
-    pos = imagesearch("./trigos/trigo2.png", confidence)
-    while pos[0] != -1:
-        print("trigo 2 in position : ", pos[0], pos[1])
-        pyautogui.click(x=pos[0], y=pos[1], clicks=1, button='left')
-        pyautogui.moveTo(cursorposition)
-        time.sleep(6)
-        if imagesearch("./trigos/trigo2.png", confidence) != pos:
-            pos = imagesearch("./trigos/trigo2.png", confidence)
-        else:
-            break
-
-    pos = imagesearch("./trigos/trigo3.png", confidence)
-    while pos[0] != -1:
-        print("trigo 3 in position : ", pos[0], pos[1])
-        pyautogui.click(x=pos[0], y=pos[1], clicks=1, button='left')
-        pyautogui.moveTo(cursorposition)
-        time.sleep(6)
-        if imagesearch("./trigos/trigo3.png", confidence) != pos:
-            pos = imagesearch("./trigos/trigo3.png", confidence)
-        else:
-            break
-
-    pos = imagesearch("./trigos/trigo4.png", confidence)
-    while pos[0] != -1:
-        print("trigo 4 in position : ", pos)
-        pyautogui.click(x=pos[0], y=pos[1], clicks=1, button='left')
-        pyautogui.moveTo(cursorposition)
-        time.sleep(6)
-        if imagesearch("./trigos/trigo4.png", confidence) != pos:
-            pos = imagesearch("./trigos/trigo4.png", confidence)
-        else:
-            break
-
-    movements.moveright()
-    time.sleep(7)
+    if found_flag:
+        for i in results:
+            if results[i] != [-1, -1]:
+                print("trigo em", results[i])
+                pos = results[i]
+                pyautogui.click(x=pos[0], y=pos[1], clicks=1, button='left')
+                pyautogui.moveTo(cursorposition)
+                time.sleep(4)
+                break
+    else:
+        movements.moveright()
+        time.sleep(7)
 print("image not found")
