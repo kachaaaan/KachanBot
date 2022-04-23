713233b9aa11d988d1e0aca3370bc2408a389af4
Kachan <luizkachan@gmail.com>
2022-04-23 03:01:57 -0300

    initial

--- pastatest.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-from python_imagesearch.imagesearch import imagesearch
-from python_imagesearch.imagesearch import imagesearch_from_folder
-from pathlib import Path
-datafolder = "trigos/"
-
-results = str(imagesearch_from_folder(datafolder, 0.9))
-print(results)
